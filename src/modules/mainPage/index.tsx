b0140ef063d8ab9861cd177bb3c8e8dd6cba8ea5
Romchik Stakhiv <Roma.Stakhiv@gmail.com>
2024-01-15 00:27:37 +0200

    feature/starterLoader

--- src/modules/mainPage/index.tsx
+++ src/modules/mainPage/index.tsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TiresInput } from './components/TiresInput';
 import { GoogleMap } from './components/GoogleMap';
 import { Box } from '@mui/material';
 import { OurServices } from './components/OurServices';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { actions } from './reducer';
+import { selectFilterData } from './selectors';
+import Loader from '../../shared/components/Loader';
 
 export function MainPage() {
   const dispatch = useDispatch();
+  /*   const filtersParams = useSelector(selectFilterData());
+  const [isLoading, setIsLoading] = useState(true); */
 
   useEffect(() => {
     dispatch(actions.getFilterData());
   }, [dispatch]);
+
+  /* useEffect(() => {
+    const allDataLoaded = Object.values(filtersParams).every(
+      (array) => array.length > 0,
+    );
+    if (allDataLoaded) {
+      setIsLoading(false);
+    }
+  }, [filtersParams]); */
 
   return (
     <Box>
