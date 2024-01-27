a03940e1e42f42e8a0ef1ceaa5e3b48c65da0313
Romchik Stakhiv <Roma.Stakhiv@gmail.com>
2024-01-27 21:57:00 +0200

    renameCentralOffices

--- src/modules/mainPage/components/GoogleMap.tsx
+++ src/modules/mainPage/components/GoogleMap.tsx
@@ -161,7 +161,7 @@ export function GoogleMap() {
                   fontSize: '1.1rem',
                 },
               }}>
-              Глобал
+              {t('tireCenterGlobal')}: {t('headerAddress')}
             </Typography>
             <Typography
               variant="body1"
@@ -174,7 +174,7 @@ export function GoogleMap() {
                   fontSize: '1.1rem',
                 },
               }}>
-              Tyre Plus
+              {t('tireCenterTyrePlus')}: {t('headerAddress2')}
             </Typography>
           </Stack>
           <Stack>
